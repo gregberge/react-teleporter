c0d6c6a9de28d6dc36ce1d16f69566495a4e7e45
Yuya Tanaka <yuya.presto@gmail.com>
2020-01-30 17:59:56 +0900

    Add typescript declaration

---
+++ types/index.d.ts
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+interface TargetComponentType {
+  <C extends React.ElementType = 'div'>(props: { as?: C } & Omit<React.ComponentPropsWithoutRef<C>, 'as'>): JSX.Element
+}
+
+export interface TeleporterType {
+  Source: React.ComponentType
+  Target: TargetComponentType
+  useTargetRef(): (element: Element | null) => void
+}
+
+export function createTeleporter(): TeleporterType
