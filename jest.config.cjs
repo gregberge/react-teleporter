66952ed16730b879c5834ce7856110904741e4d4
Greg Bergé <berge.greg@gmail.com>
2022-06-18 16:56:22 +0200

    chore: update format & contributing

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,4 @@
 /* eslint-env node */
 module.exports = {
-  testEnvironment: 'jsdom',
-}
+  testEnvironment: "jsdom",
+};
--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,15 +1,15 @@
 /* eslint-env jest */
 
-import * as React from 'react'
-import { render, cleanup, fireEvent } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
-import { createTeleporter, Teleporter as TypeTeleporter } from './index'
+import * as React from "react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { createTeleporter, Teleporter as TypeTeleporter } from "./index";
 
-afterEach(cleanup)
+afterEach(cleanup);
 
-describe('teleporter', () => {
-  it('teleports any children into target', () => {
-    const Teleporter = createTeleporter()
+describe("teleporter", () => {
+  it("teleports any children into target", () => {
+    const Teleporter = createTeleporter();
 
     const { getByTestId } = render(
       <div>
@@ -19,18 +19,18 @@ describe('teleporter', () => {
         <div>
           <Teleporter.Source>Hello</Teleporter.Source>
         </div>
-      </div>,
-    )
+      </div>
+    );
 
-    expect(getByTestId('target')).toHaveTextContent('Hello')
-  })
+    expect(getByTestId("target")).toHaveTextContent("Hello");
+  });
 
-  it('teleports context', () => {
-    const Teleporter = createTeleporter()
-    const SimpleContext = React.createContext<string>('')
+  it("teleports context", () => {
+    const Teleporter = createTeleporter();
+    const SimpleContext = React.createContext<string>("");
     const SimpleContextDump: React.FC = () => {
-      return <>{React.useContext(SimpleContext)}</>
-    }
+      return <>{React.useContext(SimpleContext)}</>;
+    };
 
     const { getByTestId } = render(
       <div>
@@ -44,14 +44,14 @@ describe('teleporter', () => {
             </Teleporter.Source>
           </div>
         </SimpleContext.Provider>
-      </div>,
-    )
+      </div>
+    );
 
-    expect(getByTestId('target')).toHaveTextContent('Hello')
-  })
+    expect(getByTestId("target")).toHaveTextContent("Hello");
+  });
 
   it('supports "as" on target', () => {
-    const Teleporter = createTeleporter()
+    const Teleporter = createTeleporter();
 
     const { getByTestId } = render(
       <div>
@@ -61,17 +61,17 @@ describe('teleporter', () => {
         <div>
           <Teleporter.Source>Hello</Teleporter.Source>
         </div>
-      </div>,
-    )
+      </div>
+    );
 
-    expect(getByTestId('target')).toHaveTextContent('Hello')
+    expect(getByTestId("target")).toHaveTextContent("Hello");
     // @ts-ignore
-    expect(getByTestId('target').firstChild.tagName).toBe('HEADER')
-  })
+    expect(getByTestId("target").firstChild.tagName).toBe("HEADER");
+  });
 
-  it('forwards props to Target', () => {
-    const Teleporter = createTeleporter()
-    const clickSpy = jest.fn()
+  it("forwards props to Target", () => {
+    const Teleporter = createTeleporter();
+    const clickSpy = jest.fn();
 
     const { getByTestId } = render(
       <div>
@@ -81,22 +81,22 @@ describe('teleporter', () => {
         <div>
           <Teleporter.Source>Hello</Teleporter.Source>
         </div>
-      </div>,
-    )
+      </div>
+    );
 
-    const targetContainer = getByTestId('target')
-    expect(targetContainer).toHaveTextContent('Hello')
+    const targetContainer = getByTestId("target");
+    expect(targetContainer).toHaveTextContent("Hello");
     // @ts-ignore
-    fireEvent.click(targetContainer.firstChild)
-    expect(clickSpy).toHaveBeenCalled()
-  })
+    fireEvent.click(targetContainer.firstChild);
+    expect(clickSpy).toHaveBeenCalled();
+  });
 
   it('expose "useTargetRef"', () => {
-    const Teleporter = createTeleporter()
+    const Teleporter = createTeleporter();
 
     function CustomTarget() {
-      const ref = Teleporter.useTargetRef()
-      return <section ref={ref} />
+      const ref = Teleporter.useTargetRef();
+      return <section ref={ref} />;
     }
 
     const { getByTestId } = render(
@@ -107,19 +107,19 @@ describe('teleporter', () => {
         <div>
           <Teleporter.Source>Hello</Teleporter.Source>
         </div>
-      </div>,
-    )
+      </div>
+    );
 
-    expect(getByTestId('target')).toHaveTextContent('Hello')
+    expect(getByTestId("target")).toHaveTextContent("Hello");
     // @ts-ignore
-    expect(getByTestId('target').firstChild.tagName).toBe('SECTION')
-  })
+    expect(getByTestId("target").firstChild.tagName).toBe("SECTION");
+  });
 
-  describe('mount order', () => {
-    let Teleporter: TypeTeleporter
-    let App: React.FC<{ hasTarget: boolean; hasSource: boolean }>
+  describe("mount order", () => {
+    let Teleporter: TypeTeleporter;
+    let App: React.FC<{ hasTarget: boolean; hasSource: boolean }>;
     beforeEach(() => {
-      Teleporter = createTeleporter()
+      Teleporter = createTeleporter();
       App = ({ hasTarget, hasSource }) => {
         return (
           <div>
@@ -128,86 +128,86 @@ describe('teleporter', () => {
               {hasSource && <Teleporter.Source>Hello</Teleporter.Source>}
             </div>
           </div>
-        )
-      }
-    })
+        );
+      };
+    });
 
-    it('supports target late mount', () => {
+    it("supports target late mount", () => {
       const { getByTestId, rerender } = render(
-        <App hasSource hasTarget={false} />,
-      )
-      const targetContainer = getByTestId('target')
-      expect(targetContainer).not.toHaveTextContent('Hello')
-      rerender(<App hasSource hasTarget />)
-      expect(targetContainer).toHaveTextContent('Hello')
-    })
+        <App hasSource hasTarget={false} />
+      );
+      const targetContainer = getByTestId("target");
+      expect(targetContainer).not.toHaveTextContent("Hello");
+      rerender(<App hasSource hasTarget />);
+      expect(targetContainer).toHaveTextContent("Hello");
+    });
 
-    it('supports target unmount', () => {
-      const { getByTestId, rerender } = render(<App hasSource hasTarget />)
-      const targetContainer = getByTestId('target')
-      expect(targetContainer).toHaveTextContent('Hello')
-      rerender(<App hasSource hasTarget={false} />)
-      expect(targetContainer).not.toHaveTextContent('Hello')
-    })
+    it("supports target unmount", () => {
+      const { getByTestId, rerender } = render(<App hasSource hasTarget />);
+      const targetContainer = getByTestId("target");
+      expect(targetContainer).toHaveTextContent("Hello");
+      rerender(<App hasSource hasTarget={false} />);
+      expect(targetContainer).not.toHaveTextContent("Hello");
+    });
 
-    it('supports source late mount', () => {
+    it("supports source late mount", () => {
       const { getByTestId, rerender } = render(
-        <App hasSource={false} hasTarget />,
-      )
-      const targetContainer = getByTestId('target')
-      expect(targetContainer).not.toHaveTextContent('Hello')
-      rerender(<App hasSource hasTarget />)
-      expect(targetContainer).toHaveTextContent('Hello')
-    })
+        <App hasSource={false} hasTarget />
+      );
+      const targetContainer = getByTestId("target");
+      expect(targetContainer).not.toHaveTextContent("Hello");
+      rerender(<App hasSource hasTarget />);
+      expect(targetContainer).toHaveTextContent("Hello");
+    });
 
-    it('supports source unmount', () => {
-      const { getByTestId, rerender } = render(<App hasSource hasTarget />)
-      const targetContainer = getByTestId('target')
-      expect(targetContainer).toHaveTextContent('Hello')
-      rerender(<App hasSource={false} hasTarget />)
-      expect(targetContainer).not.toHaveTextContent('Hello')
-    })
+    it("supports source unmount", () => {
+      const { getByTestId, rerender } = render(<App hasSource hasTarget />);
+      const targetContainer = getByTestId("target");
+      expect(targetContainer).toHaveTextContent("Hello");
+      rerender(<App hasSource={false} hasTarget />);
+      expect(targetContainer).not.toHaveTextContent("Hello");
+    });
 
-    it('supports source swapping', () => {
+    it("supports source swapping", () => {
       function App() {
-        const [source, setSource] = React.useState('A')
+        const [source, setSource] = React.useState("A");
         return (
           <div>
             <div data-testid="target">
               <Teleporter.Target />
             </div>
-            <button type="button" onClick={() => setSource('A')}>
+            <button type="button" onClick={() => setSource("A")}>
               Use A
             </button>
-            <button type="button" onClick={() => setSource('B')}>
+            <button type="button" onClick={() => setSource("B")}>
               Use B
             </button>
             <div>
-              {source === 'A' && (
+              {source === "A" && (
                 <Teleporter.Source>Source A</Teleporter.Source>
               )}
-              {source === 'B' && (
+              {source === "B" && (
                 <Teleporter.Source>Source B</Teleporter.Source>
               )}
             </div>
           </div>
-        )
+        );
       }
-      const { getByTestId, getByText } = render(<App />)
-      const targetContainer = getByTestId('target')
-      const useABtn = getByText('Use A')
-      const useBBtn = getByText('Use B')
-      expect(targetContainer).toHaveTextContent('Source A')
-      fireEvent.click(useBBtn)
-      expect(targetContainer).toHaveTextContent('Source B')
-      fireEvent.click(useABtn)
-      expect(targetContainer).toHaveTextContent('Source A')
-    })
-  })
+      const { getByTestId, getByText } = render(<App />);
+      const targetContainer = getByTestId("target");
+      const useABtn = getByText("Use A");
+      const useBBtn = getByText("Use B");
+      expect(targetContainer).toHaveTextContent("Source A");
+      fireEvent.click(useBBtn);
+      expect(targetContainer).toHaveTextContent("Source B");
+      fireEvent.click(useABtn);
+      expect(targetContainer).toHaveTextContent("Source A");
+    });
+  });
 
-  describe('multiple sources / targets', () => {
-    it('takes only the latest defined source', () => {
-      const Teleporter = createTeleporter()
+  describe("multiple sources / targets", () => {
+    it("takes only the latest defined source", () => {
+      const Teleporter = createTeleporter();
 
       const { getByTestId } = render(
         <div>
@@ -218,14 +218,14 @@ describe('teleporter', () => {
             <Teleporter.Source>A</Teleporter.Source>
             <Teleporter.Source>B</Teleporter.Source>
           </div>
-        </div>,
-      )
+        </div>
+      );
 
-      expect(getByTestId('target')).toHaveTextContent('B')
-    })
+      expect(getByTestId("target")).toHaveTextContent("B");
+    });
 
-    it('takes uses the latest source if the new one is unmounted', () => {
-      const Teleporter = createTeleporter()
+    it("takes uses the latest source if the new one is unmounted", () => {
+      const Teleporter = createTeleporter();
 
       const { getByTestId, rerender } = render(
         <div>
@@ -236,10 +236,10 @@ describe('teleporter', () => {
             <Teleporter.Source>A</Teleporter.Source>
             <Teleporter.Source>B</Teleporter.Source>
           </div>
-        </div>,
-      )
+        </div>
+      );
 
-      expect(getByTestId('target')).toHaveTextContent('B')
+      expect(getByTestId("target")).toHaveTextContent("B");
 
       rerender(
         <div>
@@ -249,14 +249,14 @@ describe('teleporter', () => {
           <div>
             <Teleporter.Source>A</Teleporter.Source>
           </div>
-        </div>,
-      )
+        </div>
+      );
 
-      expect(getByTestId('target')).toHaveTextContent('A')
-    })
+      expect(getByTestId("target")).toHaveTextContent("A");
+    });
 
-    it('allows multiple sources using `multiSources` option', () => {
-      const Teleporter = createTeleporter({ multiSources: true })
+    it("allows multiple sources using `multiSources` option", () => {
+      const Teleporter = createTeleporter({ multiSources: true });
 
       const { getByTestId } = render(
         <div>
@@ -267,14 +267,14 @@ describe('teleporter', () => {
             <Teleporter.Source>A</Teleporter.Source>
             <Teleporter.Source>B</Teleporter.Source>
           </div>
-        </div>,
-      )
+        </div>
+      );
 
-      expect(getByTestId('target')).toHaveTextContent('AB')
-    })
+      expect(getByTestId("target")).toHaveTextContent("AB");
+    });
 
-    it('handles uses the latest target defined', () => {
-      const Teleporter = createTeleporter()
+    it("handles uses the latest target defined", () => {
+      const Teleporter = createTeleporter();
 
       const { getByTestId } = render(
         <div>
@@ -287,11 +287,11 @@ describe('teleporter', () => {
           <div>
             <Teleporter.Source>A</Teleporter.Source>
           </div>
-        </div>,
-      )
+        </div>
+      );
 
-      expect(getByTestId('targetA')).not.toHaveTextContent('A')
-      expect(getByTestId('targetB')).toHaveTextContent('A')
-    })
-  })
-})
+      expect(getByTestId("targetA")).not.toHaveTextContent("A");
+      expect(getByTestId("targetB")).toHaveTextContent("A");
+    });
+  });
+});
